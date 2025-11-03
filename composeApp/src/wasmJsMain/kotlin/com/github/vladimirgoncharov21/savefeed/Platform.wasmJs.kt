8c1de92b07ebf2690d5e9e84fc5ae55aaf4ef677
Vladimir Goncharov <77740993+Vladimir-Goncharov21@users.noreply.github.com>
2025-11-03 13:45:42 +0300

    remove deafult code (#4)

--- composeApp/src/wasmJsMain/kotlin/com/github/vladimirgoncharov21/savefeed/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package com.github.vladimirgoncharov21.savefeed
-
-class WasmPlatform : Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
