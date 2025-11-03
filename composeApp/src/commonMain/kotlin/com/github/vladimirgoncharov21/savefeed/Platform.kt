a6b812fe908723dc2dbe00909e9cb3b0da5b959a
Vladimir Goncharov <goncharovvladimir19@gmail.com>
2025-11-03 13:43:27 +0300

    remove deafult code

--- composeApp/src/commonMain/kotlin/com/github/vladimirgoncharov21/savefeed/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.github.vladimirgoncharov21.savefeed
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/jvmMain/kotlin/com/github/vladimirgoncharov21/savefeed/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package com.github.vladimirgoncharov21.savefeed
-
-class JVMPlatform : Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
